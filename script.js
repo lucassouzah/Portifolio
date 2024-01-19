863692403deb3b5dd47da7448b13f7b123e4b549
lucas souza <lucassouzaluciano12@gmail.com>
2024-01-19 16:28:51 -0300

    atualizacao portifolio p2

---
+++ script.js
@@ -0,0 +1,31 @@
+window.sr = ScrollReveal ({reset: true});
+
+sr.reveal('.scroll-title-primary',{
+    rotate: {x: 0,y: 80, z:0},
+    duration:2000
+});
+
+sr.reveal('.scroll-paragrafo-primary',{
+    rotate: {x: 0,y: 80, z:0},
+    duration:2500
+});
+
+sr.reveal('.scroll-btn-primary',{
+    rotate: {x: 0,y: 100, z:0},
+    duration:3000
+});
+
+
+
+sr.reveal('.img-topo-site',{
+    rotate: {x: 100,y: 100, z:0},
+    duration:2000,
+    delay:500
+})
+
+sr.reveal('.habilidades',{
+    rotate: {x: 0,y: 80, z:0},
+    duration:2000,
+    delay:500
+})
+
