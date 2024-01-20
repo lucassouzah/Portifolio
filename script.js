00a494f17df558d5a0929b26136268d2053bf8fa
lucas souza <lucassouzaluciano12@gmail.com>
2024-01-20 15:49:16 -0300

    att js

--- script.js
+++ script.js
@@ -20,39 +20,39 @@ sr.reveal('.scroll-btn-primary',{
 sr.reveal('.img-topo-site',{
     rotate: {x: 100,y: 100, z:0},
     duration:2000,
-    delay:500
+    delay:200
 })
 
 sr.reveal('.habilidades',{
     rotate: {x: 0,y: 80, z:0},
     duration:2000,
-    delay: 150
+    delay: 100
     
 })
 
 sr.reveal('.img-sobre',{
     rotate: {x: 0,y: 100, z:100},
     duration:2000,
-    delay:500
+    delay:200
     
 })
 sr.reveal('.titulo-sobre',{
     rotate: {x: 0,y: 80, z:0},
     duration:2000,
-    delay:200
+    delay:100
 })
 
 sr.reveal('.paragrafo-sobre',{
     rotate: {x: 0,y: 80, z:0},
     duration:2500,
-    delay:200
+    delay:100
     
 })
 
 sr.reveal('.btn-social',{
     rotate: {x: 0,y: 80, z:0},
     duration:3000,
-    delay:200
+    delay:100
     
 })
 
