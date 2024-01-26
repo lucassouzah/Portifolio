c1508e74db381918c4db7f98884923b6dfc17a69
lucas souza <lucassouzaluciano12@gmail.com>
2024-01-26 18:03:17 -0300

    validando com js

---
+++ validate.js
@@ -0,0 +1,14 @@
+const form = document.querySelector("#form");
+const nomeInput = document.querySelector("#name");
+const emailInput = document. querySelector("#email");
+const mensagemTextarea = document.querySelector("#mensagem");
+
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+
+    // Verifica se o nome está vazio
+    if(nomeInput.value === ""){
+        
+    }
+
+})
