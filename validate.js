fe2af48bdaf9bd6de1f5fbe98dd8fbef1337cebb
lucas souza <lucassouzaluciano12@gmail.com>
2024-02-07 21:49:20 -0300

    validacao js

--- validate.js
+++ validate.js
@@ -1,63 +1,43 @@
-class FormSubmit {
-    constructor(settings) {
-      this.settings = settings;
-      this.form = document.querySelector(settings.form);
-      this.formButton = document.querySelector(settings.button);
-      if (this.form) {
-        this.url = this.form.getAttribute("action");
-      }
-      this.sendForm = this.sendForm.bind(this);
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const messageTextarea = document.querySelector("#message");
+
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+
+    // Verifica se o nome está vazio
+    if(nameInput.value === "") {
+        alert("Por Favor, preencha o seu nome");
+        return;
     }
-  
-    displaySuccess() {
-      this.form.innerHTML = this.settings.success;
+    // Verifica se o email esta correto
+    if(emailInput.value === "" || !isEmailValid(emailInput.value)){
+        alert("Por favor, preencha o seu email");
+        return;
     }
-  
-    displayError() {
-      this.form.innerHTML = this.settings.error;
+
+    // Verifica se o campo de mensagem foi preenchido
+    if (messageTextarea.value ==="") {
+        alert("Por favor, escreva uma mensagem");
+        return;
     }
-  
-    getFormObject() {
-      const formObject = {};
-      const fields = this.form.querySelectorAll("[name]");
-      fields.forEach((field) => {
-        formObject[field.getAttribute("name")] = field.value;
-      });
-      return formObject;
-    }
-    
-    onSubmission(event) {
-      event.preventDefault();
-      event.target.disabled = true;
-      event.target.innerText = "Enviando...";
-    }
-  
-    async sendForm(event) {
-      try {
-        this.onSubmission(event);
-        await fetch(this.url, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-          body: JSON.stringify(this.getFormObject()),
-        });
-        this.displaySuccess();
-      } catch (error) {
-        this.displayError();
-        throw new Error(error);
-      }
-    }
-  
-    init() {
-      if (this.form) this.formButton.addEventListener("click", this.sendForm);
-      return this;
-    }
-  }
-  const formSubmit = new FormSubmit({
-    form: "[data-form]",
-    button: "[data-button]",
-    success: "<h1 class='success'>Mensagem enviada!</h1>",
-    error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
-  });
+
+
+    // Se os campos estiverem correntos então enviar formulario
+    form.submit();
+
+});
+
+// função para validar o email
+function isEmailValid(email) {
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+    if (emailRegex.text(email)){
+        return true;
+    }    
+
+    return false;
+
+}
